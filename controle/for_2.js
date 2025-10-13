4432c43d87ae97d3194527952925854890b274fd
Pablo Farias <pablofdeafarias268@gmail.com>
2025-10-13 10:05:43 -0300

    Mais alteracoes

---
+++ controle/for_2.js
@@ -0,0 +1,11 @@
+//for(;;){
+//}
+
+//for(; true; ){
+//}
+let controle = 1;
+for (; controle <= 10; ) {
+    console.log(controle);
+    controle++
+}
+console.log('Fim!');
---
+++ controle/for_3.js
@@ -0,0 +1,6 @@
+//laco for  que imprima no console do valor 7 ate o valor 3
+
+for (let i = 7; i >= 3; i--) {
+  console.log(i);
+}       
+console.log('Fim!');
