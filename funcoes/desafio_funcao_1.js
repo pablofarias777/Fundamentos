407eb944635adf7857dcdc25c85bbb0eb3fc7700
Pablo Farias <pablofarias@MacBook-Air-de-Pablo.local>
2026-03-03 12:37:53 -0300

    Aprendendo funcao

---
+++ funcoes/desafio_funcao_1.js
@@ -0,0 +1,22 @@
+// criar uma funcao que vai recber 2 paramentros -> console.log para somar os dois paramentros(+)
+// funcao 2 paramentros -> console.log para subtrair os dois paramentros(-)
+
+// ...(3, 4)
+// ...(31,98)
+
+// ... (11,7)
+// ... (19,52)
+
+function somar(a, b) {
+    console.log(a + b);
+}
+
+somar(3, 4);
+somar(31, 98);
+
+function subtrair(c, d) {
+    console.log(c - d);
+}
+
+subtrair(11, 7);
+subtrair(19, 52);
